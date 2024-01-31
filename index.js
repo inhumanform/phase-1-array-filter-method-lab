aa39f6127e9633e9bd6216259e0a4d7be6123566
Drew <dch528@gmail.com>
2024-01-31 13:44:17 -0500

    Drew Hairston Array Filter Method Lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+
+
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+
+}
+
+
+function fuzzyMatch(drivers, partialName) {
+    return drivers.filter(driver => driver.startsWith(partialName));
+}
+
+// function fuzzyMatch(drivers, partialName) {
+//     return drivers.filter(driver => driver.toLowerCase().startsWith(partialName.toLowerCase()));
+
+// }
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+}
